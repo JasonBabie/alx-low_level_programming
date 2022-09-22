d04d79e522e5d53db53eb8ab463d35011a703154
Jason Babie <babiejasonrb@gmail.com>
2022-09-22 03:54:17 -0700

    Correct compiler spec

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,17 +7,18 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
+
+	int i, j, k;
+	i = 0;
 	while (*(dest + i) != '\0')
 	{
 		i++;
 	}
-	int j = 0;
+	j = 0;
 	while (*(src + j) != '\0')
 	{
 		j++;
 	}
-	int k;
 	for (k = 0; k < j; k++)
 	{
 		*(dest + i + k) = *(src + k);
