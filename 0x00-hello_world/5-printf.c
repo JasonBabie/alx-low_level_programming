95802fb838fe2815e68c2bc45e2cc5a5630ffeee
Jason Babie <babiejasonrb@gmail.com>
2022-09-24 13:03:53 -0700

    Solution to hello printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - displays a string
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
